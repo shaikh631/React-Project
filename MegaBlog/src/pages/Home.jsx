4db107a6228bb326a059f3a22cbd827415053bda
shaikh631 <as9251145@gmail.com>
2026-05-16 23:43:02 +0530

    Update

--- MegaBlog/src/pages/Home.jsx
+++ MegaBlog/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, {useEffect , useState} from 'react'
 import service from '../appwrite/config'
 import { Container , PostCard } from '../components'
 
+
 function Home() {
     const [posts , setpost] = useState(null)
 
--- MegaBlog/vite.config.js
+++ MegaBlog/vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
      tailwindcss()
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+
 })
